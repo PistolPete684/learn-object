2dc65d297651836465b25cdc677bc250c8168f17
StraightforwardDigital <111469465+StraightforwardDigital@users.noreply.github.com>
2025-06-22 12:47:50 -0300

    adding js file

---
+++ main.js
@@ -0,0 +1,7 @@
+function animal(obj){
+  // use concatination to join peices of a string with peices of an object
+  return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+}
+
+// arrow function format
+// const animal = (obj) => `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
